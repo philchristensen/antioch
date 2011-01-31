77536da1bba4d20f83b7406abbfdf1bc52741aa6
Phil Christensen <phil@bubblehouse.org>
2011-01-30 22:46:02 -0500

    fixes for observation bugs

--- src/antioch/assets/webroot/js/client.js
+++ src/antioch/assets/webroot/js/client.js
@@ -48,61 +48,64 @@ antioch.ClientConnector.methods(
 		$('.name').html(observations['name']);
 		$('.description').html(observations['description']);
 		
-		var contents_area = $('.contents');
 		var player_content = $('<div id="players-list"></div>');
 		var item_content = $('<div id="contents-list"></div>');
+		
 		if(observations['contents'].length){
 			var player_list = $('<ul></ul>');
 			var contents_list = $('<ul></ul>');
 			
-			for(key in observations['contents']){
-				(function(){
-					var details = observations['contents'][key];
-					if(details.type > 0){
-						image_div = ($('<div class="player-image"></div>'));
-						if(details.image){
-							image_div.append($('<img src="' + details.image + '" />'));
-						}
-						else{
-							image_div.append($('<img src="/assets/images/silhouette.png" />'));
-						}
-					
-						name_div = $('<div class="player-name"></div>');
-						name_div.html(details.name);
-					
-						list_item = $('<li></li>');
-						list_item.click(function(){
-							look(details.name);
-						});
-						list_item.append(image_div);
-						list_item.append(name_div);
-					
-						if(details.mood){
-							mood_div = $('<div class="player-mood"></div>');
-							mood_div.html(details.mood);
-							list_item.append(mood_div);
-						}
-						player_list.append(list_item);
+			for(index in observations['contents']){
+				var details = observations['contents'][index];
+				if(details.type){
+					image_div = ($('<div class="player-image"></div>'));
+					if(details.image){
+						image_div.append($('<img src="' + details.image + '" />'));
 					}
 					else{
-						list_item = $('<li onClick="look(this.innerHTML)"></li>');
-						list_item.html(details.name);
-						list_item.click(function(){
-							look(details.name);
-						});
-						contents_list.append(list_item);
+						image_div.append($('<img src="/assets/images/silhouette.png" />'));
 					}
-				})()
+				
+					name_div = $('<div class="player-name"></div>');
+					name_div.html(details.name);
+				
+					list_item = $('<li></li>');
+					list_item.click(function(){
+						look(details.name);
+					});
+					list_item.append(image_div);
+					list_item.append(name_div);
+				
+					if(details.mood){
+						mood_div = $('<div class="player-mood"></div>');
+						mood_div.html(details.mood);
+						list_item.append(mood_div);
+					}
+					player_list.append(list_item);
+				}
+				else{
+					list_item = $('<li></li>');
+					list_item.html(details.name);
+					list_item.click(function(){
+						look(details.name);
+					});
+					contents_list.append(list_item);
+				}
 			}
 			
-			player_content.append($('<strong>people here:</strong>'));
-			player_content.append(player_list);
-			player_content.append($('<br style="clear: both;"/>'))
-			
-			item_content.append($('<strong>obvious contents:</strong>'));
-			item_content.append(contents_list);
-			item_content.append($('<br style="clear: both;"/>'))
+			if(player_list[0].children.length){
+				player_content.append($('<strong>people here:</strong>'));
+				player_content.append(player_list);
+				player_content.append($('<br style="clear: both;"/>'))
+			}
+			if(contents_list[0].children.length){
+				item_content.append($('<strong>obvious contents:</strong>'));
+				item_content.append(contents_list);
+				item_content.append($('<br style="clear: both;"/>'))
+			}
 		}
+
+		var contents_area = $('.contents');
 		contents_area.empty();
 		contents_area.append(player_content);
 		contents_area.append(item_content);
