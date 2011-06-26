473f517af91c1ecf4c9572e880a3defbe2fdc1d4
Phil Christensen <phil@bubblehouse.org>
2011-06-25 21:49:39 -0400

    continuing admin work

--- src/antioch/dj/settings.py
+++ src/antioch/dj/settings.py
@@ -130,17 +130,32 @@ INSTALLED_APPS = (
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
+    'formatters': {
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
     'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
+        'console':{
+            'level':'DEBUG',
+            'class':'logging.StreamHandler',
+            'formatter': 'simple'
+        },
     },
     'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
+        'django': {
+            'handlers':['console'],
             'propagate': True,
+            'level':'INFO',
         },
+        'django.request': {
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+        'antioch.dj.custom': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        }
     }
 }
