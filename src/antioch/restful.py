217b0a3ff5cbe62d7a542eb09b06908271ae616b
Phil Christensen <pchristensen@freelancersunion.org>
2011-10-04 20:38:43 -0400

    first success with rest rsrc

--- src/antioch/restful.py
+++ src/antioch/restful.py
@@ -8,32 +8,31 @@
 Provide a local REST-style interface to the transaction layer.
 """
 
+from twisted.internet import defer
 from twisted.web import resource
 
 import simplejson
+
+# wget --load-cookies=cookies.dat http://localhost:8888/actions/parse -S --post-data="{\"test\":\"test\"}"
+
+# cookies.dat:
+# localhost	TRUE	/	FALSE	1333585359	sid	bd5e02cadf1d9bce4c1dc85c0a542887
 
 class TransctionInterface(resource.Resource):
 	def __init__(self, user, segments):
 		self.user = user
 		self.segments = segments
 	
-	def render_GET(self, request):
-		return """
-			<form action="/actions/%(action)s" method="post">
-			<input type="submit" />
-			</form>
-		""" % dict(
-			action = '/'.join(self.segments)
-		)
-	
+	@defer.inlineCallbacks
 	def render_POST(self, request):
 		f = getattr(self, 'transact_%s' % self.segments[0], None)
 		if(f is None):
-			return 404
+			raise 404
 		
-		options = simplejson.loads(request.content.getvalue())
-		
-		return defer.maybeDeferred(f, options)
+		json = request.content.getvalue()
+		options = simplejson.loads(json)
+		result = yield defer.maybeDeferred(f, options)
+		defer.returnValue(simplejson.dumps(result))
 	
 	def transact_parse(self, options):
-		print options
+		return {'result':'success'}
