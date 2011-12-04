7c603a3734556948fe716953f48969044b562a8c
Phil Christensen <pchristensen@freelancersunion.org>
2011-12-04 11:22:04 -0500

    cleaned up django urls

--- src/antioch/client/urls.py
+++ src/antioch/client/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls.defaults import patterns, url
 
-urlpatterns = patterns('',
-	url(r'login/', 'django.contrib.auth.views.login', {'template_name': 'login.html'}, name='login'),
+urlpatterns = patterns('django.contrib.auth.views',
+	url(r'login/', 'login', {'template_name': 'login.html'}, name='login'),
 ) + patterns('antioch.client.views',
+	url(r'assets/(?P<path>.*)$', 'serve_static'),
 	url(r'logout/', 'logout', name='logout'),
 	url(r'', 'client', name='client'),
 )
--- src/antioch/urls.py
+++ src/antioch/urls.py
@@ -5,7 +5,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-	url(r'^assets/(?P<path>.*)$', 'antioch.client.views.serve_static'),
 	url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 	url(r'^admin/', include(admin.site.urls)),
 	url(r'', include('antioch.client.urls')),
