9107f2600de47cce4b3959d94ed652d7387de6a9
Phil Christensen <pchristensen@logicworks.net>
2017-10-19 15:46:13 -0400

    remove default versions of files

--- antioch/settings/development.py
+++
@@ -1,5 +0,0 @@
-from .docker import *
-
-DB_URL = "psycopg2://%(USER)s:%(PASSWORD)s@%(HOST)s/%(NAME)s" % DATABASES['default']
-DB_URL_DEFAULT = "psycopg2://%(USER)s:%(PASSWORD)s@%(HOST)s/%(NAME)s" % DATABASES['default']
-
