e78d23205c7ef9b9a54493ba67e28d4f8b8af4fb
Phil Christensen <phil@bubblehouse.org>
2019-05-07 12:15:47 -0400

    making progress on a more mobile-friendly editor

--- antioch/plugins/editors/static/js/editor-plugin.js
+++ antioch/plugins/editors/static/js/editor-plugin.js
@@ -9,73 +9,16 @@
   less.sheets.push($('link[href="/static/less/editors.less"]')[0]);
   less.refresh();
   
-  var editorsPanel = $('<div>').attr({
-    "class":   "editors-panel"
-  });
-  
-  var tabList = $('<ul>').attr({
-    "class":   "nav nav-tabs",
-    "data-tabs": "tabs"
-  });
-  editorsPanel.append(tabList);
-  
-  var tabContent = $('<div>').attr({
-    "class":   "tab-content"
-  });
-  editorsPanel.append(tabContent);
-  
-  $('.page-container').append(editorsPanel);
-  
   $(document).antioch('addMessageListener', 'edit', function(msg){
-    var tabContentId = "edit-tab-content-" + msg.details.kind.replace('/', '-') + '-' + msg.details.id;
-    var tabId = "edit-tab-" + msg.details.kind.replace('/', '-') + '-' + msg.details.id;
-    var closeTab = function(){
-      $('#' + tabId).remove();
-      $('#' + tabContentId).remove();
-    };
-
-    var newTabContent = $('<div>').attr({
-      'id': tabContentId,
-      'class': "tab-pane"
-    });
-    newTabContent.load('/editor/' + msg.details.kind + '/' + msg.details.id);
-    
-    var newTab = $('<a>').attr({
-      'id': tabId,
-      'href':  "#" + tabContentId,
-      'data-toggle': "tab"
-    }).html(msg.details.__str__);
-    
-    newTab.append($("<a>").attr({
-      'class': 'close-tab glyphicon glyphicon-remove'
-    }).click(closeTab));
-    
-    tabList.append($("<li>").append(newTab));
-    tabContent.append(newTabContent);
-    
-    $(document).tab('show');
-  });
-})(jQuery);
-
-function setupAjaxForm(formQuery){
-  formQuery.submit(function(){
-    $.ajax({
-      type: $(this).attr('method'),
-      url: $(this).attr('action'),
-      data: $(this).serialize(),
-      success: function(data){
-        $(this).find('.form-group').removeClass('error');
-        console.log('Success.')
-      },
-      error: function(jqXHR, textStatus, errorThrown){
-        errors = jQuery.parseJSON(jqXHR.responseText);
-        $(this).find('.form-group').removeClass('error');
-        for(var fieldName in errors){
-          var formGroup = $('#id_' + fieldName).parents('.form-group')
-          formGroup.addClass('error');
-        }
+    $.get('/editor/' + msg.details.kind + '/' + msg.details.id, function(response, status, xhr) {
+      if(status == "error"){
+        var msg = "Couldn't load editor: "+ xhr.status + " " + xhr.statusText;
+        $('#connection').antioch('write', msg, true);
+      }
+      else {
+        var front = document.getElementById('observations');
+        var back = flippant.flip(front, response);
       }
     });
-    return false;
   });
-};
+})(jQuery);
