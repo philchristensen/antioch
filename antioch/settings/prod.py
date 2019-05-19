b7d197a87181b4f831b6008bb96e27ccaca0e497
Phil Christensen <phil@bubblehouse.org>
2019-05-18 20:50:25 -0400

    working on production env

---
+++ antioch/settings/prod.py
@@ -0,0 +1,30 @@
+import os
+
+from .base import *
+
+CSRF_TRUSTED_ORIGINS = ['localhost']
+
+if os.environ.get('ROLE') in ('celeryflower', 'worker', 'beat'):
+    DEBUG = False
+    ALLOWED_HOSTS += ['testserver']
+
+STATIC_ROOT = "/usr/src/app/static"
+
+CELERY_BROKER_URL = 'redis://redis:6379/0'
+CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
+
+DATABASES['default'].update({
+    "HOST": os.environ['DB_HOST'],
+    "PORT": os.environ['DB_PORT'],
+    "NAME": "antioch",
+    "USER": "antioch",
+    "PASSWORD": os.environ['DB_PASSWD'],
+    'OPTIONS': {
+        'sslmode': 'require',
+    }
+})
+
+CACHES['default'].update({
+    "BACKEND": "django.core.cache.backends.memcached.MemcachedCache",
+    "LOCATION": os.environ['MEMCACHE']
+})
